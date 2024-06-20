04038e475ad3132dfd0bb8f232d65a1818a40417
André Victor <andrehsvictor@gmail.com>
2024-06-20 10:19:39 -0300

    chore: Refactor App component and add search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,135 @@
+import Header from "./components/header";
+import ProductList from "./components/product-list";
+import ProductItem from "./components/product-item";
 import useFetchProducts from "./hooks/useFetchProducts";
+import { useState } from "react";
+import EditModal from "./components/edit-modal";
+import { Product } from "./types/Product";
+import useEditProduct from "./hooks/useEditProduct";
+import useDeleteProduct from "./hooks/useDeleteProduct";
+import AddModal from "./components/add-modal";
+import useAddProduct from "./hooks/useAddProduct";
 
-export default function App() {
-  const { products, isLoading, error } = useFetchProducts();
+const App = () => {
+  const { products, setTrigger, trigger } = useFetchProducts();
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [editProduct, setEditProduct] = useState<Product>({
+    id: 0,
+    name: "",
+    price: 0,
+    quantity: 0,
+    description: "",
+  });
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+
+  const handleEditProduct = useEditProduct(editProduct);
+
+  const handleDeleteProduct = useDeleteProduct();
+
+  const handleSearch = (search: string) => {
+    if (search === "") {
+      return;
+    } else {
+      const filtered = products.filter((product) =>
+        product.name.toLowerCase().includes(search.toLowerCase())
+      );
+      setFilteredProducts(filtered);
+    }
+  };
+
+  const handleAddProduct = useAddProduct();
+
+  const renderProducts = () => {
+    if (search === "") {
+      return products.map((product) => (
+        <ProductItem
+          key={product.id}
+          product={product}
+          onDelete={(id) => {
+            handleDeleteProduct(id);
+            setTrigger(trigger + 1);
+          }}
+          onEdit={() => {
+            setShowEditModal(true);
+            setEditProduct(product);
+          }}
+        />
+      ));
+    } else {
+      return filteredProducts.map((product) => (
+        <ProductItem
+          key={product.id}
+          product={product}
+          onDelete={(id) => {
+            handleDeleteProduct(id);
+            setTrigger(trigger + 1);
+          }}
+          onEdit={() => {
+            setShowEditModal(true);
+            setEditProduct(product);
+          }}
+        />
+      ));
+    }
+  };
 
   return (
-    <div className="flex flex-col items-center h-screen bg-gray-200 p-4">
-      <h1 className="text-4xl font-bold">Welcome</h1>
-      <p className="text-lg mt-2">to Clickmart product management system</p>
-      <ul className="mt-4">
-        <li>
-          {products.map((product: any) => (
-            <div
-              key={product.id}
-              className="flex items-center bg-white shadow-md rounded-md p-2 my-2"
-            >
-              <div className="ml-4">
-                <h2 className="text-lg font-bold">{product.name}</h2>
-                <p className="text-sm">{product.description}</p>
-                <p className="text-sm">Price: {product.price}</p>
-              </div>
-            </div>
-          ))}
-        </li>
-      </ul>
-    </div>
+    <>
+      <div className="flex flex-col items-center h-screen bg-slate-300">
+        <Header />
+        <div className="flex justify-end w-1/2">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+              handleSearch(e.target.value);
+            }}
+            className="w-full p-2 border border-gray-300 rounded-md"
+          />
+        </div>
+        <div className="flex justify-end w-1/2 mt-4">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            onClick={() => setShowAddModal(true)}
+          >
+            Add a product
+          </button>
+        </div>
+        <ProductList>
+          <>{renderProducts()}</>
+        </ProductList>
+      </div>
+      {showEditModal && (
+        <EditModal
+          isOpen={showEditModal}
+          onClose={() => setShowEditModal(false)}
+          onSave={(product) => {
+            console.log(product);
+            handleEditProduct(product);
+            setTrigger(trigger + 1);
+            setShowEditModal(false);
+          }}
+          product={editProduct}
+        />
+      )}
+      {showAddModal && (
+        <AddModal
+          isOpen={showAddModal}
+          onClose={() => setShowAddModal(false)}
+          onSave={(product) => {
+            console.log(product);
+            handleAddProduct(product);
+            setTrigger(trigger + 1);
+            setShowAddModal(false);
+          }}
+        />
+      )}
+    </>
   );
-}
+};
+
+export default App;
