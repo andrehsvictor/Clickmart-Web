c43613e8096a388e02ad7b6d47a76d911a9cdb4b
André Victor <andrehsvictor@gmail.com>
2024-06-20 10:19:54 -0300

    chore: Add Header component for displaying app title and description

---
+++ src/components/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold mt-2">Clickmart products&#128293;</h1>
+      <p className="text-xl mt-2">
+        This is a simple app to display products from an API
+      </p>
+    </>
+  );
+}
