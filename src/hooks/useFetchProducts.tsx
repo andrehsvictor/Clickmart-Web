f3264adffc239d5bb2ee24f70cc8194a8eb904f4
André Victor <andrehsvictor@gmail.com>
2024-06-20 10:21:12 -0300

    chore: Update useFetchProducts to fetch products from the API

--- src/hooks/useFetchProducts.tsx
+++ src/hooks/useFetchProducts.tsx
@@ -1,17 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Product } from "../types/Product";
 
 const useFetchProducts = () => {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [trigger, setTrigger] = useState(0);
   const url = import.meta.env.VITE_API_URL;
 
   useEffect(() => {
     axios
       .get(`${url}/products`)
       .then((response) => {
-        console.log(response.data);
         setProducts(response.data);
         setIsLoading(false);
       })
@@ -19,9 +20,9 @@ const useFetchProducts = () => {
         setError(error);
         setIsLoading(false);
       });
-  }, []);
+  }, [trigger]);
 
-  return { products, isLoading, error };
+  return { products, isLoading, error, setTrigger, trigger };
 };
 
 export default useFetchProducts;
