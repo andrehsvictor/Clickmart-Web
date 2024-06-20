0b78445ebee2eea3a936105e49571a6f4d9070ce
André Victor <andrehsvictor@gmail.com>
2024-06-20 10:48:40 -0300

    chore: Update AddModal component to include form validation and error handling

--- src/components/add-modal.tsx
+++ src/components/add-modal.tsx
@@ -16,6 +16,9 @@ export default function AddModal({ isOpen, onClose, onSave }: AddModalProps) {
   const [price, setPrice] = useState(0);
   const [quantity, setQuantity] = useState(0);
   const [description, setDescription] = useState("");
+  const [disabled, setDisabled] = useState(false);
+  const [nameError, setNameError] = useState(false);
+  const [priceError, setPriceError] = useState(false);
 
   return (
     isOpen && (
@@ -23,7 +26,7 @@ export default function AddModal({ isOpen, onClose, onSave }: AddModalProps) {
         className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-10`}
       >
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-lg shadow-md">
-          <h2 className="text-2xl font-bold mb-4">Edit Product</h2>
+          <h2 className="text-2xl font-bold mb-4">Add Product</h2>
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -45,9 +48,26 @@ export default function AddModal({ isOpen, onClose, onSave }: AddModalProps) {
                 type="text"
                 id="name"
                 value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full p-2 border border-gray-300 rounded-md"
+                onChange={(e) => {
+                  if (e.target.value.length < 1) {
+                    setNameError(true);
+                    setDisabled(true);
+                    setName(e.target.value);
+                  } else {
+                    setNameError(false);
+                    setDisabled(false);
+                    setName(e.target.value);
+                  }
+                }}
+                className={`w-full p-2 border border-gray-300 rounded-md ${
+                  nameError ? "border-red-500" : ""
+                }`}
               />
+              {nameError && (
+                <p className="text-red-500 text-sm">
+                  Name is required &#x26A0;
+                </p>
+              )}
             </div>
             <div className="mb-4">
               <label htmlFor="price" className="block text-lg font-semibold">
@@ -57,9 +77,26 @@ export default function AddModal({ isOpen, onClose, onSave }: AddModalProps) {
                 type="number"
                 id="price"
                 value={price}
-                onChange={(e) => setPrice(+e.target.value)}
-                className="w-full p-2 border border-gray-300 rounded-md"
+                onChange={(e) => {
+                  if (+e.target.value < 1) {
+                    setPriceError(true);
+                    setDisabled(true);
+                    setPrice(+e.target.value);
+                  } else {
+                    setPriceError(false);
+                    setDisabled(false);
+                    setPrice(+e.target.value);
+                  }
+                }}
+                className={`w-full p-2 border border-gray-300 rounded-md ${
+                  priceError ? "border-red-500" : ""
+                }`}
               />
+              {priceError && (
+                <p className="text-red-500 text-sm">
+                  Price must be greater than 0 &#x26A0;
+                </p>
+              )}
             </div>
             <div className="mb-4">
               <label htmlFor="quantity" className="block text-lg font-semibold">
@@ -97,7 +134,8 @@ export default function AddModal({ isOpen, onClose, onSave }: AddModalProps) {
               </button>
               <button
                 type="submit"
-                className="bg-blue-500 text-white px-4 py-2 rounded-md ml-4"
+                disabled={disabled}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md ml-4 disabled:opacity-50 disabled:cursor-not-allowed"
               >
                 Save
               </button>
